dce43353dcafe7b67148e7cde8b22e6c9d3051fa
Mohit <2021pcs5380@mnit.ac.in>
2025-04-20 15:15:33 +0530

    Auth router done with prisma set up

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -7,11 +9,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello Guys welcome to leetlab🔥");
 });
 
+app.use("/api/v1/auth", authRoutes);
+
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on port 8081");
+  console.log("Server is running on port ", PORT);
 });
